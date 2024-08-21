0ab40822c733e0f43cb821b15b525d9b427327c6
steevedevops <barthelemysteevebernard@gmail.com>
2024-08-21 20:59:02 -0300

    On branch v5.0.0  Changes to be committed: 	modified:   lib/src/utils/bestapp-utils.dart :x

--- lib/src/utils/bestapp-utils.dart
+++ lib/src/utils/bestapp-utils.dart
@@ -12,7 +12,8 @@ class BestappUtils {
 
   /// Converter o valor de uma String com `R$`
   static double converterMoedaParaDouble(String valor) {
-    assert(valor.isNotEmpty);
+    // assert(valor.isNotEmpty);
+    if(valor.isNotEmpty) return 0;
     final value = double.tryParse(valor.replaceAll('R\$', '').replaceAll('.', '').replaceAll(',', '.'));
     return value ?? 0;
   }
