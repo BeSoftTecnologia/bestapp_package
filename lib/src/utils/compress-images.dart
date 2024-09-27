a2ccb5098346b53553d1c6aa42f43af5bdd982d2
steevedevops <barthelemysteevebernard@gmail.com>
2024-09-27 17:27:02 -0300

    On branch v5.0.3-wasm  Changes to be committed: 	modified:   lib/src/utils/compress-images.dart 	modified:   pubspec.lock 	modified:   pubspec.yaml

--- lib/src/utils/compress-images.dart
+++ lib/src/utils/compress-images.dart
@@ -1,11 +1,11 @@
-import 'dart:io';
-import 'package:flutter_image_compress/flutter_image_compress.dart';
-import 'package:path_provider/path_provider.dart';
+// import 'dart:io';
+// import 'package:flutter_image_compress_web/flutter_image_compress_web.dart';
+// import 'package:path_provider/path_provider.dart';
 
-Future <File?> compressImages(String pathImages, int qualityImage) async {
-  print("Start compress");
-  var tmpDir = (await getTemporaryDirectory()).path;
-  final target = "$tmpDir/${DateTime.now().millisecondsSinceEpoch}.jpg";
-  XFile? imagecompress = await FlutterImageCompress.compressAndGetFile(pathImages, target, quality: qualityImage);
-  return File(imagecompress!.path);
-}
+// Future <File?> compressImages(String pathImages, int qualityImage) async {
+//   print("Start compress");
+//   var tmpDir = (await getTemporaryDirectory()).path;
+//   final target = "$tmpDir/${DateTime.now().millisecondsSinceEpoch}.jpg";
+//   XFile? imagecompress = await FlutterImageCompressWeb.compressAndGetFile(pathImages, target, quality: qualityImage);
+//   return File(imagecompress!.path);
+// }
